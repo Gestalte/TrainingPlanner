881edc592b860633b130216d4504a66e826db9a3
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-02-15 00:32:29 +0200

    Add database and initial migration.

---
+++ TrainingPlanner.Data/Migrations/20230214223110_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingPlanner.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Weekday = table.Column<short>(type: "INTEGER", nullable: false),
+                    Timeslot = table.Column<short>(type: "INTEGER", nullable: false),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.ScheduleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exercise",
+                columns: table => new
+                {
+                    ExerciseId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    ScheduleId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercise", x => x.ExerciseId);
+                    table.ForeignKey(
+                        name: "FK_Exercise_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "ScheduleId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercise_ScheduleId",
+                table: "Exercise",
+                column: "ScheduleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercise");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
---
+++ TrainingPlanner.Data/TrainingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrainingPlanner.Data
+{
+    public class TrainingDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=training.db");
+        }
+
+        public DbSet<Schedule> Schedules { get; set; }
+    }
+
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public string Title { get; set; }
+        public Int16 Weekday { get; set; }
+        public Int16 Timeslot { get; set; }
+        public ICollection<Exercise> Exercises { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+    public class Exercise
+    {
+        public int ExerciseId { get; set; }
+        public string Description { get; set; }
+    }
+}
