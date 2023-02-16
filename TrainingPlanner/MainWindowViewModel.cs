4e8833377768a892251dc73f2ec5b3716c717956
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-02-16 19:03:14 +0200

    Small clean up.

--- TrainingPlanner/MainWindowViewModel.cs
+++ TrainingPlanner/MainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -96,7 +95,7 @@ namespace TrainingPlanner
         }
 
         [ObservableProperty]
-        ObservableCollection<ExerciseItem> exerciseItems = new ObservableCollection<ExerciseItem>();
+        ObservableCollection<ExerciseItem> exerciseItems = new();
 
         [ObservableProperty]
         private string mainTitle = "";
@@ -147,14 +146,13 @@ namespace TrainingPlanner
 
             Title = selectedSchedule?.Title ?? "";
             AmpmSelection = (TimeSlot)selectedSchedule!.Timeslot;
-
             WeekDaySelection = (WeekDay)selectedSchedule.Weekday;
-
-            var exercises = selectedSchedule.Exercises.Select(s => new ExerciseItem(s.Description, RemoveExcerciseItemCommand));
-
-            exercises.ToList().ForEach(f => ExerciseItems.Add(f));
-
             ItemCompleted = selectedSchedule.IsComplete;
+
+            selectedSchedule.Exercises
+                .Select(s => new ExerciseItem(s.Description, RemoveExcerciseItemCommand))
+                .ToList()
+                .ForEach(f => ExerciseItems.Add(f));
         }
 
         [RelayCommand]
@@ -290,54 +288,6 @@ namespace TrainingPlanner
             SetWeekdayCheckboxesToFalse();
             ExerciseDescription = "";
             ExerciseItems = new ObservableCollection<ExerciseItem>();
-        }
-
-
-    }
-
-    public class ScheduleBuilder : IScheduleBuilder
-    {
-        private readonly IScheduleRepository scheduleRepository;
-        public IRelayCommand<object> EditCommand { get; set; } = null!; // Use property injection to set this.
-
-        public ScheduleBuilder(IScheduleRepository scheduleRepository)
-        {
-            this.scheduleRepository = scheduleRepository;
-        }
-
-        public List<WeekItem> LoadSchedules()
-        {
-            Func<Schedule, WeekItem> makeWeekItem = s
-                => new WeekItem(s.Title, s.ScheduleId, EditCommand, s.Exercises.Select(s => s.Description).ToList());
-
-            if (EditCommand == null)
-            {
-                throw new ArgumentNullException(nameof(EditCommand));
-            }
-
-            var weekItemArr = new WeekItem[14];
-
-            for (int i = 0; i < weekItemArr.Length; i++)
-            {
-                weekItemArr[i] = new WeekItem();
-            }
-
-            this.scheduleRepository.GetAll()
-                .Where(w => w.IsComplete == false)
-                .DistinctBy(d => (d.Weekday, d.Timeslot))
-                .ToList()
-                .ForEach(f => AddItem(ref weekItemArr, makeWeekItem(f), (WeekDay)f.Weekday, (TimeSlot)f.Timeslot));
-
-            return weekItemArr.ToList();
-        }
-
-        private WeekItem[] AddItem(ref WeekItem[] weekItems, WeekItem newItem, WeekDay weekDay, TimeSlot timeSlot)
-        {
-            int index = (int)timeSlot == 1 ? (int)weekDay : (int)weekDay + 7;
-
-            weekItems[index] = newItem;
-
-            return weekItems;
         }
     }
 }
---
+++ TrainingPlanner/ScheduleBuilder.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TrainingPlanner.Data;
+
+namespace TrainingPlanner
+{
+    public class ScheduleBuilder : IScheduleBuilder
+    {
+        private readonly IScheduleRepository scheduleRepository;
+        public IRelayCommand<object> EditCommand { get; set; } = null!; // Use property injection to set this.
+
+        public ScheduleBuilder(IScheduleRepository scheduleRepository)
+        {
+            this.scheduleRepository = scheduleRepository;
+        }
+
+        public List<WeekItem> LoadSchedules()
+        {
+            Func<Schedule, WeekItem> makeWeekItem = s
+                => new WeekItem(s.Title, s.ScheduleId, EditCommand, s.Exercises.Select(s => s.Description).ToList());
+
+            if (EditCommand == null)
+            {
+                throw new ArgumentNullException(nameof(EditCommand));
+            }
+
+            var weekItemArr = new WeekItem[14];
+
+            for (int i = 0; i < weekItemArr.Length; i++)
+            {
+                weekItemArr[i] = new WeekItem();
+            }
+
+            this.scheduleRepository.GetAll()
+                .Where(w => w.IsComplete == false)
+                .DistinctBy(d => (d.Weekday, d.Timeslot))
+                .ToList()
+                .ForEach(f => AddItem(ref weekItemArr, makeWeekItem(f), (WeekDay)f.Weekday, (TimeSlot)f.Timeslot));
+
+            return weekItemArr.ToList();
+        }
+
+        private WeekItem[] AddItem(ref WeekItem[] weekItems, WeekItem newItem, WeekDay weekDay, TimeSlot timeSlot)
+        {
+            int index = (int)timeSlot == 1 ? (int)weekDay : (int)weekDay + 7;
+
+            weekItems[index] = newItem;
+
+            return weekItems;
+        }
+    }
+}
